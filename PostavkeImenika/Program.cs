c6de57925bfa5ab402d36ca8203ac63aba2538ee
Sinisa Vojtek <sinisa.vojtek@vsite.hr>
2016-10-13 21:23:27 +0200

    Defaultni namespace

---
+++ PostavkeImenika/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MojaKlasa1 mk1 = new Csharp.MojaKlasa1();
+            MojaKlasa2 mk2 = new Csharp.MojaKlasa2();
+            Console.WriteLine("Bok");
+        }
+    }
+}
