f16e183b339747144930e92274109c158ca8b7d0
Sinisa Vojtek <sinisa.vojtek@vsite.hr>
2016-10-17 14:33:18 +0200

    Operater Is - provjera kompatibilnosti

--- OperatorIs/Program.cs
+++ OperatorIs/Program.cs
@@ -14,8 +14,16 @@ namespace CSharp.TipoviOperatori
             
             Osoba osoba = new Osoba();
 
-
+            if (janko is Osoba)
+                Console.WriteLine("Janko je osoba.");
+            if (osoba is Student)
+                Console.WriteLine("Osoba je student.");
+            if (janko is Student)
+                Console.WriteLine("Janko je student.");
+            if (osoba is Osoba)
+                Console.WriteLine("Osoba je osoba.");
             Console.ReadKey();
+
         }
     }
 }
