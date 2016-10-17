1db3095769a99b3ab78d55f64b86843966425286
Sinisa Vojtek <sinisa.vojtek@vsite.hr>
2016-10-17 14:13:12 +0200

    Inicijalizacija članova strukture - int to double cast...

--- var/Program.cs
+++ var/Program.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System;
+
 namespace CSharp.TipoviOperatori
 {
     struct S
@@ -11,6 +12,23 @@ namespace CSharp.TipoviOperatori
     {
         static void Main(string[] args)
         {
+
+            var i0 = 5;
+            var d0 = 5.2;
+            i0 = (int)d0;
+
+            int i1 = 5;
+            double d1 = 5.2;
+            i1 = (int)d1;
+
+            var s=new S();
+
+            Console.WriteLine("Vrijednost a:");
+            s.a= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Vrijednost b:");
+            s.b= Convert.ToInt32(Console.ReadLine());
+
+            Console.ReadKey();
 
         }
     }
