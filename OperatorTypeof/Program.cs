93712896a112b0523653736392f8b4dde8ae30ba
Sinisa Vojtek <sinisa.vojtek@vsite.hr>
2016-10-20 17:55:29 +0200

    TypeOf...informacije o metodama i članovima

--- OperatorTypeof/Program.cs
+++ OperatorTypeof/Program.cs
@@ -8,12 +8,22 @@ namespace CSharp.TipoviOperatori
         static void Main(string[] args)
         {
             Type t = typeof(System.DateTime);
+            
 
+           foreach (MemberInfo mi in t.GetMembers())
+           {
+                
+               Console.WriteLine($"{mi.Name} {mi.MemberType.ToString()}");
+              
 
-            //foreach (MemberInfo mi in t.GetMembers())
-            //{
-            //    Console.WriteLine($"{mi.Name} {mi.MemberType.ToString()}");
-            //}
+            }
+
+            foreach (System.Reflection.MethodInfo mInfo in t.GetMethods())
+            {
+                Console.WriteLine(mInfo.ToString());
+                Console.WriteLine("Members:");
+                System.Reflection.MemberInfo[] memberInfo = t.GetMembers();
+            }
 
             foreach (MethodInfo mi in t.GetMethods())
             {
